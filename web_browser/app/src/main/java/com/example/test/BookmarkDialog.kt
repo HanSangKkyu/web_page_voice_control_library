ece1195a9f2f795424655815d82bbc29cc7b5aae
한상규 <kkyu67@naver.com>
2021-07-14 22:48:09 +0900

    Update BookmarkDialog.kt

--- web_browser/app/src/main/java/com/example/test/BookmarkDialog.kt
+++ web_browser/app/src/main/java/com/example/test/BookmarkDialog.kt
@@ -2,12 +2,17 @@ package com.example.test
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Build
 import android.view.Window
 import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class BookmarkDialog(context : Context) {
     private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
+    private val sharedPref = context?.getSharedPreferences("bookmark", Context.MODE_PRIVATE)
 
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     fun start() {
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
         dlg.setContentView(R.layout.dialog_bookmark)     //다이얼로그에 사용할 xml 파일을 불러옴
@@ -19,7 +24,66 @@ class BookmarkDialog(context : Context) {
             dlg.dismiss()
         }
 
-
         dlg.show()
+
+
+//        with (sharedPref.edit()) {
+//            putString("bookmark", "")
+//            commit()
+//        }
+//
+//        Log.e("bookmark",getBookmarks().toString()+" "+getBookmarks().size.toString())
+//        addBookmark("naver.com")
+//        addBookmark("nate.com")
+//        Log.e("bookmark",getBookmarks().toString()+" "+getBookmarks().size.toString())
+//        removeBookmark("naver.com")
+//        Log.e("bookmark",getBookmarks().toString()+" "+getBookmarks().size.toString())
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun getBookmarks(): ArrayList<String> {
+        val bookmark = sharedPref.getString("bookmark", "")
+        var res = ArrayList<String>()
+        val arr = bookmark?.split(',')
+        for(i in arr!!){
+            if(i != ""){
+                res.add(i.toString())
+            }
+        }
+        return res
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun addBookmark(newItem:String){
+        var al = getBookmarks()
+        al.add(newItem)
+
+        var res = ""
+        for(i in al){
+            res += i+","
+        }
+
+        with (sharedPref.edit()) {
+            putString("bookmark", res.substring(0,res.length-1))
+            commit()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun removeBookmark(item:String){
+        var al = getBookmarks()
+        al.remove(item)
+
+        var res = ""
+        for(i in al){
+            res += i+","
+        }
+
+        with (sharedPref.edit()) {
+            putString("bookmark", res.substring(0,res.length-1))
+            commit()
+        }
     }
 }
