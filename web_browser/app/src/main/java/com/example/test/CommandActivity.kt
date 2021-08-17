17eb94eb72e8a4e3362409cf6b973ce24f93fcbe
한상규 <kkyu67@naver.com>
2021-08-17 18:04:30 +0900

    make edit, remove command

--- web_browser/app/src/main/java/com/example/test/CommandActivity.kt
+++ web_browser/app/src/main/java/com/example/test/CommandActivity.kt
@@ -11,6 +11,7 @@ import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_command.*
+import kotlinx.android.synthetic.main.dialog_bookmark.*
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -18,6 +19,9 @@ class CommandActivity : AppCompatActivity() {
 
     var url = ""
     var selectedFun = ""
+    var selectedItemPos = -1
+    var commandList = ArrayList<Command>()
+    var funList = ArrayList<String>()
 
     @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,15 +32,17 @@ class CommandActivity : AppCompatActivity() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     fun init() {
         // get extra
         var intent = getIntent()
-        url = intent.getStringExtra("url").toString()
+        url = getHostPartInUrl(intent.getStringExtra("url").toString())
+        urlTextView.setText(url)
         var funStr = intent.getStringExtra("fun").toString()
 
 
         // get function list in this page
-        var funList = ArrayList<String>()
+        funList = ArrayList<String>()
         for (i in funStr.split(',')) {
             if (i !in DefaultFunVO().getDefaultFun()) {
                 funList.add(i)
@@ -54,7 +60,6 @@ class CommandActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-//                Log.e("asdf", funSpinner.getItemAtPosition(position).toString())
                 selectedFun = funSpinner.getItemAtPosition(position).toString()
             }
 
@@ -65,9 +70,55 @@ class CommandActivity : AppCompatActivity() {
 
 
         // set listView
-        url = getHostPartInUrl(url)
+        refreshListView()
+
+
+        // set create Btn
+        createBtn.setOnClickListener { v ->
+            addCommand(url, line.text.toString(), selectedFun)
+            line.setText("")
+            Toast.makeText(this, "생성 완료", Toast.LENGTH_SHORT).show()
+            refreshListView()
+        }
+
+        // set save Btn
+        saveBtn.setOnClickListener { v ->
+            editCommandItem()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    private fun editCommandItem() {
+        var sharedPref = getSharedPreferences("command", Context.MODE_PRIVATE)
+
+        var json = getCommand()
+        for (i in 0..json.length() - 1) {
+            var tmp_url = json.getJSONObject(i).getString("url")
+            if (tmp_url.equals(url)) {
+                var tmp_command = json.getJSONObject(i).getJSONArray("command")
+                removeItem(selectedItemPos)
+
+                var tmp_command_item = JSONObject()
+                tmp_command_item.put("line", line.text.toString())
+                tmp_command_item.put("function", selectedFun)
+
+                tmp_command.put(selectedItemPos, tmp_command_item)
+                break
+            }
+        }
+
+        with(sharedPref.edit()) {
+            putString("command", json.toString())
+            commit()
+        }
+
+        refreshListView()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    private fun refreshListView() {
         val jArr = getCommandOfUrl(url)
-        var commandList = ArrayList<Command>()
+        commandList = ArrayList<Command>()
         for (i in 0..jArr.length() - 1) {
             commandList.add(
                 Command(
@@ -78,14 +129,55 @@ class CommandActivity : AppCompatActivity() {
         }
         customCommandListView.adapter = MyCustomAdapter(this, commandList)
 
-        // set create Btn
-        createBtn.setOnClickListener { v ->
-            addCommand(url, line.text.toString(), selectedFun)
-            line.setText("")
-            Toast.makeText(this, "생성 완료", Toast.LENGTH_SHORT).show()
-        }
+
+        // 삭제
+        customCommandListView.onItemLongClickListener =
+            AdapterView.OnItemLongClickListener OnKeyListener@{ parent, view, position, id ->
+                removeItem(position)
+                return@OnKeyListener true
+            }
+
+        // 수정 모드
+        customCommandListView.onItemClickListener =
+            AdapterView.OnItemClickListener OnKeyListener@{ parent, view, position, id ->
+                loadItemDataOnView(position)
+            }
     }
 
+    private fun loadItemDataOnView(position: Int) {
+        selectedItemPos = position
+        line.setText(getCommandOfUrl(url).getJSONObject(position).getString("line"))
+        selectedFun = getCommandOfUrl(url).getJSONObject(position).getString("function")
+        for (i in 0..funList.size - 1) {
+            if (funList.get(i).equals(selectedFun)) {
+                funSpinner.setSelection(i)
+                break
+            }
+        }
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun removeItem(position: Int) {
+        var sharedPref = getSharedPreferences("command", Context.MODE_PRIVATE)
+
+        var json = getCommand()
+        for (i in 0..json.length() - 1) {
+            var tmp_url = json.getJSONObject(i).getString("url")
+            if (tmp_url.equals(url)) {
+                var tmp_command = json.getJSONObject(i).getJSONArray("command")
+                tmp_command.remove(position)
+            }
+        }
+
+        with(sharedPref.edit()) {
+            putString("command", json.toString())
+            commit()
+        }
+
+        refreshListView()
+    }
 
     fun getCommand(): JSONArray {
 
@@ -157,9 +249,7 @@ class CommandActivity : AppCompatActivity() {
 
     fun getHostPartInUrl(url: String): String {
         var res = url.substring(url.indexOf("://") + 3)
-//        url = url.substring(url.indexOf(".")+1,url.indexOf("/"))
         res = res.substring(0, res.indexOf("/"))
-        Log.e("asdf", res)
         return res
     }
 }
@@ -199,4 +289,5 @@ private class MyCustomAdapter(context: Context, data: ArrayList<Command>) : Base
 
         return rowMain
     }
+
 }
