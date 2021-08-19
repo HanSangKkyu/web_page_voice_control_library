4d5328da5ac1e3fa8adc8457a18faafc48e96e36
한상규 <kkyu67@naver.com>
2021-08-19 14:14:00 +0900

    Update MainActivity.kt

--- web_browser/app/src/main/java/com/example/test/MainActivity.kt
+++ web_browser/app/src/main/java/com/example/test/MainActivity.kt
@@ -166,6 +166,9 @@ class MainActivity : AppCompatActivity() {
 
         // 사용자 명령어 관리 페이지
         commandBtn.setOnClickListener { v ->
+            if(frList.isEmpty()){
+                return@setOnClickListener
+            }
 
             var intent = Intent(this, CommandActivity::class.java)
             intent.putExtra("url", getNowUrl())
